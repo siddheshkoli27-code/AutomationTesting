2e205ebc0c3931746826231db7888685f67c7fba
siddheshkoli27-code <siddheshkoli19@gmail.com>
2025-12-30 18:46:36 +0530

    commit

---
+++ src/Practice/Test1.java
@@ -0,0 +1,13 @@
+package Practice;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=15;
+		int c=a+b;
+		System.out.println("Addition of number is:" + c);
+
+	}
+
+}
